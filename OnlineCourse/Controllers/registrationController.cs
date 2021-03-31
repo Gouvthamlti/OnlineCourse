441c0149c1746958addba14285c120277daff933
Gouvtham <Gouvtham.Natarajan@lntinfotech.com>
2021-03-31 15:35:18 +0530

    webapi

---
+++ OnlineCourse/Controllers/registrationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using OnlineCourse.Models;
+
+
+namespace OnlineCourse.Controllers
+{
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class registrationController : ApiController
+    {
+        onlinecourseEntities entities = new onlinecourseEntities();
+
+    }
+}
